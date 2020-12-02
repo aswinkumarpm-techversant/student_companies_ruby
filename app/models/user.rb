3831408ed0863c9d1ada3bd025a21bc3bbbe93bc
Aswin kumar P M <aswinkumarpm3@gmail.com>
2020-12-02 20:56:00 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  enum role: { student: 0, teachers: 1 }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
