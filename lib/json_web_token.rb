8900c9b3108736370b675452a0075bc4ea85ad76
Aswin kumar P M <aswinkumarpm3@gmail.com>
2020-12-07 14:22:32 +0530

    some tests

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,5 +1,7 @@
 class JsonWebToken
   def self.encode(payload)
+    expiration = (2).minutes.from_now.to_i
+    payload[:exp] = expiration
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
