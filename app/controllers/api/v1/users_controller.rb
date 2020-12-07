a2bd655546ad4ca01c31f1ff2691cf6e24376619
Aswin kumar P M <aswinkumarpm3@gmail.com>
2020-12-07 14:34:37 +0530

    some bug fixes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,22 @@
 class Api::V1::UsersController < ApiController
-  before_action :authorized, only: [:auto_login]
+  before_action :authenticate_request!, only: [:auto_login]
 
   # REGISTER
-  def create
-    @user = User.create(user_params)
-    if @user.valid?
-      token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token}
-    else
-      render json: {error: "Invalid username or password"}
-    end
-  end
+  # def create
+  #   @user = User.create(user_params)
+  #   if @user.valid?
+  #     token = encode_token({user_id: @user.id})
+  #     render json: {user: @user, token: token}
+  #   else
+  #     render json: {error: "Invalid username or password"}
+  #   end
+  # end
 
   # LOGGING IN
   def login
     @user = User.find_by(email: params[:email])
     if @user && @user.valid_password?(params[:password])
-      token = encode_token({user_id: @user.id})
+      token = JsonWebToken.encode({user_id: @user.id})
       render json: {user: @user, token: token}
     else
       render json: {error: "Invalid username or password"}
@@ -25,12 +25,12 @@ class Api::V1::UsersController < ApiController
 
 
   def auto_login
-    render json: @user
+    render json: @current_user
   end
 
-  private
-
-  def user_params
-    params.permit(:email, :password)
-  end
+  # private
+  #
+  # def user_params
+  #   params.permit(:email, :password)
+  # end
 end
