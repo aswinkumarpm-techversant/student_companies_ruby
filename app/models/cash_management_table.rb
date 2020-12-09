9656870158967df05d0091e41baa796bdc52e7c0
Aswin kumar P M <aswinkumarpm3@gmail.com>
2020-12-09 13:26:10 +0530

    tests

--- app/models/cash_management_table.rb
+++ app/models/cash_management_table.rb
@@ -3,7 +3,7 @@ class CashManagementTable < ActiveRecord::Base
   has_many :cash_in_histories
   has_many :cash_out_histories
 
-  def self.calculate_initial_cash
+  def calculate_initial_cash
     @cash_in_histories = CashInHistory.where("cash_management_table_id = ? AND status != ?", self.id, "done").order('created_at')
     @cash_out_histories = CashOutHistory.where("cash_management_table_id = ? AND status != ?", self.id, "done").order('created_at')
     total_cash_in = @cash_in_histories.sum(:revenues_amount) + @cash_in_histories.sum(:equity_amount) + @cash_in_histories.sum(:convertible_note_amount)  + @cash_in_histories.sum(:bank_debt_amount)  + @cash_in_histories.sum(:other_amount)
