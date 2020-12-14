5937fc42e91c9d113bdc77509db3c65b251c4c05
Aswin kumar P M <aswinkumarpm3@gmail.com>
2020-12-14 17:35:36 +0530

    some updates

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,19 +1,3 @@
-class JsonWebToken
-  def self.encode(payload)
-    expiration = (2).minutes.from_now.to_i
-    payload[:exp] = expiration
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
-  end
-
-  def self.decode(token)
-    return HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.secret_key_base)[0])
-  rescue
-    nil
-  end
-end
-
-
-
 require 'jwt'
 
 class JsonWebToken
