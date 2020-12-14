3db56fee8e234ee9f83d845e82693b55b018e252
Aswin kumar P M <aswinkumarpm3@gmail.com>
2020-12-14 17:33:43 +0530

    some updates

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -11,3 +11,25 @@ class JsonWebToken
     nil
   end
 end
+
+
+
+require 'jwt'
+
+class JsonWebToken
+  def self.encode(payload, expiration = (2).minutes.from_now.to_i)
+    payload = payload.dup
+    payload[:exp] = expiration.to_i
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
+
+  def self.decode(token)
+    JWT.decode(token,  Rails.application.secrets.secret_key_base)[0]
+  rescue JWT::ExpiredSignature, JWT::DecodeError
+    false
+  end
+
+  def self.decode_to_payload(token)
+    decode(token).first.except('exp').with_indifferent_access
+  end
+end
