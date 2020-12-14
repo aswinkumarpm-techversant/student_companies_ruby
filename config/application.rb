42f11d31c27737cb177116bd394ce75b2054e689
Aswin kumar P M <aswinkumarpm3@gmail.com>
2020-12-14 19:23:11 +0530

    some updates

--- config/application.rb
+++ config/application.rb
@@ -12,17 +12,17 @@ module StudentCompanies
     config.load_defaults 6.0
     config.autoload_paths << Rails.root.join('lib')
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource(
-            '*',
-            headers: :any,
-            expose: ["Authorization"],
-            methods: [:get, :patch, :put, :delete, :post, :options,               :show]
-        )
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource(
+    #         '*',
+    #         headers: :any,
+    #         expose: ["Authorization"],
+    #         methods: [:get, :patch, :put, :delete, :post, :options,               :show]
+    #     )
+    #   end
+    # end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
